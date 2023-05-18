cf81937db0cac1aa7e8f02c9dfc9ad73bfc6bb92
Cynthia S <c.sovulewski@gmail.com>
2023-05-18 14:06:32 -0700

    setup of js vue and api calls

---
+++ Traverse/static/js/app.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: '#app',
+    delimiters: ['[[', ']]'],
+    data: {
+        about: '',
+    },
+    methods: {
+        addAbout() {
+            axios.post('api/editabout/', {
+                'about': this.about,
+            }, {
+                headers: { 'X-CSRFToken': this.token }
+            }).then(() => )
+        }
+    }
+})
