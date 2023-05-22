c338144140e549c200b1654da97915d7c387517e
Cynthia S <c.sovulewski@gmail.com>
2023-05-22 15:37:16 -0700

    users can update their info about themselves

--- Traverse/accounts/forms.py
+++ Traverse/accounts/forms.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
+from django import forms 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import (Submit, Button)
+from django.urls import reverse_lazy
+from accounts.models import *
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -10,3 +15,16 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ('username', 'email')
+
+
+class AccountEditForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(AccountEditForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.add_input(Submit('save', "Save changes", css_class = 'btn-success'))
+       
+
+    class Meta:
+        model = CustomUser
+        fields = ['location', 'about', 'profile_picture']
+
