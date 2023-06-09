71e756aef5d3efe94463169a8988bbae44b43a88
Cynthia S <c.sovulewski@gmail.com>
2023-06-09 10:18:56 -0700

    cleanup

--- Traverse/posts/forms.py
+++ Traverse/posts/forms.py
@@ -4,8 +4,6 @@ from crispy_forms.layout import (Submit, Button)
 from django.urls import reverse_lazy
 from posts.models import *
 from django.forms import inlineformset_factory
-# from crispy_forms.helper import FormHelper
-# from crispy_forms.layout import Submit
 
 class CreatePostForm(forms.ModelForm):
 
@@ -33,24 +31,9 @@ class CreatePostForm(forms.ModelForm):
             'placeholder': 'Map Link',
         }),
         }
-    def __init__(self, *args, **kwargs):
-    
-    
-        super(CreatePostForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_id='id-exampleFrom'
-        self.helper.form_class='blueForms'
-        self.helper.form_method = 'post'
-        self.helper.form_action= reverse_lazy('posts:create')
-        self.helper.add_input(Submit('submit', 'Submit', css_class = 'btn-success')) 
 
 
 class PostEditForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(PostEditForm, self).__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.add_input(Submit('save', "Save changes", css_class = 'btn-success'))
-        self.helper.add_input(Button('delete', 'Delete', onclick='window.location.href="{}"'.format('delete')))
 
     class Meta:
         model = Posts
